2a7e789de49500b452a4a8fa35dd0279ec15f6b7
Cyril MICOUD <cmicoud@vitechnology.com>
2018-03-02 16:56:20 +0100

    enhance with collection and enumeration

--- src/main/java/com/mfgeek/gb/ProtoFile.java
+++ src/main/java/com/mfgeek/gb/ProtoFile.java
@@ -4,91 +4,161 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProtoFile {
-	private static final String INDENT = "\t";
-	private String name;
-	private String javaOuterClassName = null;
-	private String packageName;
-	private int index = 1;
-	
-	private List<ProtoField> fields = new ArrayList<ProtoField>();
-	
-	
-	public void appendField(ProtoField field){
-		fields.add(field);
-	}
-	
+	private static final String			INDENT							= "\t";
+	private String									syntax;
+	private String									name;
+	private boolean									isEnum							= false;
+	private String									javaOuterClassName	= null;
+	private String									javaPackage					= null;
+	private String									packageName;
+	private int											index								= 1;
 
-	public void appendField(boolean required, String type, String name, String def){
-		fields.add(new ProtoField(required, type, name, def, index++));
+	private final List<String>			imports							= new ArrayList<>();
+	private final List<ProtoField>	fields							= new ArrayList<>();
+
+	public String getSyntax() {
+		return this.syntax;
+	}
+
+	public void setSyntax(String syntax) {
+		this.syntax = syntax;
+	}
+
+	public void appendImport(String pImport) {
+		this.imports.add(pImport);
+	}
+
+	public void appendField(ProtoField field) {
+		this.fields.add(field);
+	}
+
+	public void appendField(boolean required, boolean repeated, String type, String name, String def) {
+		this.fields.add(new ProtoField(required, repeated, type, name, def, this.index++));
 	}
 
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	public boolean isEnum() {
+		return this.isEnum;
+	}
+
+	public void setEnum(boolean pIsEnum) {
+		this.isEnum = pIsEnum;
+	}
+
 	public String getJavaOuterClassName() {
-		return javaOuterClassName;
+		return this.javaOuterClassName;
 	}
 
 	public void setJavaOuterClassName(String javaOuterClassName) {
 		this.javaOuterClassName = javaOuterClassName;
 	}
 
+	public String getJavaPackage() {
+		return this.javaPackage;
+	}
+
+	public void setJavaPackage(String javaPackage) {
+		this.javaPackage = javaPackage;
+	}
+
 	public String getPackageName() {
-		return packageName;
+		return this.packageName;
 	}
 
 	public void setPackageName(String packageName) {
 		this.packageName = packageName;
 	}
-	
-	public String toString(){
+
+	@Override
+	public String toString() {
 		StringBuffer buf = new StringBuffer();
-		appendLine(buf, "package " + packageName);
-		appendLine(buf, "option java_outer_classname = \"" + javaOuterClassName + "\"");
-		appendOpenQuote(buf, "message " + name);
-		for(ProtoField f: fields)
-			appendLine(buf, INDENT + f.toString());
+		if (this.syntax != null && !this.syntax.isEmpty()) {
+			appendLine(buf, "syntax = \"" + this.syntax + "\"");
+			appendNewLine(buf);
+		}
+		appendLine(buf, "package " + this.packageName);
+		if (!this.isEnum) {
+			appendNewLine(buf);
+			if (!this.imports.isEmpty()) {
+				for (String i : this.imports) {
+					appendLine(buf, "import \"" + i + "\"");
+				}
+			}
+		}
+		appendNewLine(buf);
+		if (this.javaPackage != null && !this.javaPackage.isEmpty()) {
+			appendLine(buf, "option java_package = \"" + this.javaPackage + "\"");
+		}
+		if (this.javaOuterClassName != null && !this.javaOuterClassName.isEmpty()) {
+			appendLine(buf, "option java_outer_classname = \"" + this.javaOuterClassName + "\"");
+		}
+		appendNewLine(buf);
+		appendOpenQuote(buf, (this.isEnum ? "enum " : "message ") + this.name);
+		appendNewLine(buf);
+		for (ProtoField f : this.fields) {
+			if (this.isEnum && "value".equals(f.name)) {
+				continue;
+			}
+			appendLine(buf, INDENT + f.toString(this.isEnum));
+			appendNewLine(buf);
+		}
 		appendClosedQuote(buf);
 		return buf.toString();
 	}
-	
-	void appendLine(StringBuffer buf, String str){
+
+	void appendNewLine(StringBuffer buf) {
+		buf.append("\n");
+	}
+
+	void appendLine(StringBuffer buf, String str) {
 		buf.append(str + ";\n");
 	}
-	
-	void appendOpenQuote(StringBuffer buf, String str){
+
+	void appendOpenQuote(StringBuffer buf, String str) {
 		buf.append(str + " {\n");
 	}
-	
-	void appendClosedQuote(StringBuffer buf){
+
+	void appendClosedQuote(StringBuffer buf) {
 		buf.append("}\n");
 	}
-	
 
-	class ProtoField{
-		boolean required = true;
-		private String type;
-		private String name;
-		private String defaultValue;
-		private int index;
-		ProtoField(boolean required, String type, String name, String defaultValue, int in){
+	class ProtoField {
+		boolean								repeated	= false;
+		boolean								required	= false;
+		private final String	type;
+		private final String	name;
+		private final String	defaultValue;
+		private final int			index;
+
+		ProtoField(boolean required, boolean repeated, String type, String name, String defaultValue, int in) {
 			this.required = required;
+			this.repeated = repeated;
 			this.type = type;
 			this.name = name;
 			this.defaultValue = defaultValue;
-			index = in;
+			this.index = in;
 		}
-		public String toString(){
-			return (required?"required":"optional") + " " + type + " " 
-					+ name + " = " + index + " " 
-					+ (defaultValue == null?"":("[ "+defaultValue+" ]"));
+
+		@Override
+		public String toString() {
+			return toString(false);
+		}
+
+		public String toString(boolean isFieldForEnum) {
+			if (isFieldForEnum) {
+				return this.name + " = " + (this.index - 1);
+			} else {
+				return (this.repeated ? "repeated" : (this.required ? "required" : "optional")) + " " + this.type + " " + this.name + " = "
+						+ this.index + " " + (this.defaultValue == null ? "" : ("[ " + this.defaultValue + " ]"));
+			}
 		}
 	}
-	
-	
+
 }
--- src/main/java/com/mfgeek/gb/ProtoGenerator.java
+++ src/main/java/com/mfgeek/gb/ProtoGenerator.java
@@ -6,7 +6,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,21 +17,14 @@ import java.util.Map;
 public class ProtoGenerator {
 	private static ProtoGenerator instance = null;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 	public static ProtoGenerator newInstance() {
-		if(instance == null)
+		if (instance == null) {
 			instance = new ProtoGenerator();
+		}
 		return instance;
 	}
 
-	private static Map<String, String> map = new HashMap<String, String>();
+	private static Map<String, String> map = new HashMap<>();
 	static {
 		map.put("int", "int32");
 		map.put("integer", "int32");
@@ -37,85 +33,126 @@ public class ProtoGenerator {
 		map.put("string", "string");
 		map.put("float", "float");
 		map.put("double", "double");
-		map.put("date", "int64");//convert date value to int64/long  TODO
+		map.put("date", "int64");// convert date value to int64/long TODO
 		map.put("boolean", "bool");
 		map.put("byte", "int32");
-		//TODO add Collection support here
+		// TODO add Collection support here
 	}
 
 	public List<ProtoFile> parse(String dir, String pack, String opackageName) {
 		System.out.println("parsing java classes in package:" + dir);
-		
+
 		List<String> ignores = getIgnores();
-		
+
 		List<ProtoFile> protos = new ArrayList<ProtoFile>();
-		File f = new File(dir + packToDir(pack));
-		if(f.exists() && f.isDirectory() && f.list().length > 0){
-			String [] javaFiles = f.list();
-			System.out.println("found " + javaFiles.length + " java classes in package ");
-			for(String j: javaFiles){
+		File f = new File(dir, packToDir(pack));
+		if (f.exists() && f.isDirectory() && f.list().length > 0) {
+			List<String> javaFiles = Arrays.asList(f.list());
+			System.out.println("found " + javaFiles.size() + " java classes in package ");
+			for (String j : javaFiles) {
 				System.out.print("processing class " + j);
-				//skip **Example.java classes
-				if(j.endsWith("Example.java")){
+				// skip **Example.java classes
+				if (j.endsWith("Example.java")) {
 					System.out.println(" -- skipped **Example.java class");
 					continue;
 				}
-				
-				if(ignores.contains(j.substring(0,j.indexOf(".java")))){
+
+				if (ignores.contains(j.substring(0, j.indexOf(".java")))) {
 					System.out.println(" -- skipped ignored files defined in ignore.txt");
 					continue;
 				}
-				System.out.println(j );
-				Class c;
+				System.out.println(j);
+				Class<?> c;
 				try {
 					c = Class.forName(pack + "." + j.substring(0, j.indexOf(".")));
-				
+
 					String name = c.getSimpleName();
-					String packageName = c.getPackage().toString();
-					
+					String packageName = c.getPackage().getName();
+
 					ProtoFile pf = new ProtoFile();
-					pf.setJavaOuterClassName(name + "Obj");
+					pf.setSyntax("proto2");
+					if (c.isEnum()) {
+						pf.setEnum(true);
+						pf.setJavaOuterClassName(name + "Enum");
+						// pf.setJavaOuterClassName('E' + name);
+					} else {
+						pf.setJavaOuterClassName(name + "Obj");
+					}
+					pf.setJavaPackage(packageName);
 					pf.setName(name);
 					pf.setPackageName(opackageName);
 
-					for(Field field: c.getDeclaredFields()){
-						//skip serialVersionUID
-						if(field.getName().equals("serialVersionUID"))
-							continue;
-//						System.out.println("add field in " + j + ":" + field.getName());
-						String type = field.getType().getSimpleName().toLowerCase();
-						if(map.containsKey(type))
-							pf.appendField(true, getProtoType(type), field.getName(), null);
-						else
-							System.out.println("WARNING: failed to handle field [" 
-										+ field.getName() +"] with type ["+ type+"]. Skipped.");
+					for (Field field : c.getDeclaredFields()) {
+						manageField(javaFiles, pf, field);
 					}
-					if((c.getName() + "Key").equals(c.getSuperclass().getName()))
-					for(Field field: c.getSuperclass().getDeclaredFields()){
-						//skip serialVersionUID
-						if(field.getName().equals("serialVersionUID"))
-							continue;
-//						System.out.println("add field in " + j + ":" + field.getName());
-						String type = field.getType().getSimpleName().toLowerCase();
-						if(map.containsKey(type))
-							pf.appendField(true, getProtoType(type), field.getName(), null);
-						else
-							System.out.println("WARNING: failed to handle field [" 
-										+ field.getName() +"] with type ["+ type+"]. Skipped.");
+					if ((c.getName() + "Key").equals(c.getSuperclass().getName())) {
+						for (Field field : c.getSuperclass().getDeclaredFields()) {
+							manageField(javaFiles, pf, field);
+						}
 					}
-					
+
 					protos.add(pf);
 				} catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
 			}
-		}
-		else{
-			
+		} else {
+
 		}
 		return protos;
+	}
+
+	/**
+	 * @param javaFiles
+	 * @param pf
+	 * @param field
+	 * @return
+	 */
+	private void manageField(List<String> javaFiles, ProtoFile pf, Field field) {
+		// skip serialVersionUID
+		if (field.getName().equals("serialVersionUID")) {
+			return;
+		}
+		// System.out.println("add field in " + j + ":" + field.getName());
+		Class<?> type = field.getType();
+		checkCustomType(javaFiles, pf, type);
+		if (checkType(type)) {
+			pf.appendField(false, false, getProtoType(type), field.getName(), null);
+		} else if (Collection.class.isAssignableFrom(field.getType())) {
+			Class<?> parameterizedType = ((Class<?>) ((ParameterizedType) field.getGenericType()).getActualTypeArguments()[0]);
+			checkCustomType(javaFiles, pf, parameterizedType);
+			pf.appendField(false, true, getProtoType(parameterizedType), field.getName(), null);
+		} else {
+			System.out.println("WARNING: failed to handle field [" + field.getName() + "] with type [" + type + "]. Skipped.");
+		}
+	}
+
+	/**
+	 * @param pClass
+	 * @return
+	 */
+	private boolean checkType(Class<?> pClass) {
+		return map.containsKey(pClass.getSimpleName().toLowerCase());
+	}
+
+	/**
+	 * @param javaFiles
+	 * @param pf
+	 * @param pType
+	 */
+	private void checkCustomType(List<String> javaFiles, ProtoFile pf, Class<?> pClass) {
+		String t = pClass.getSimpleName();
+		if (javaFiles.contains(t + ".java")) {
+			String type = t.toLowerCase();
+			// if (pClass.isEnum() && t.startsWith("E")) {
+			// t = t.substring(1);
+			// }
+			pf.appendImport(t + ".proto");
+			if (!map.containsKey(type)) {
+				map.put(type, t);
+			}
+		}
 	}
 
 	public List<String> getIgnores() {
@@ -123,38 +160,38 @@ public class ProtoGenerator {
 		try {
 			InputStream is = this.getClass().getClassLoader().getResourceAsStream("ignore.txt");
 			InputStreamReader isr = new InputStreamReader(is);
-			if(is != null && isr.ready()){
-				BufferedReader br = new BufferedReader(isr); 
-				String s; 
-				while((s = br.readLine()) != null) { 
-					ignoreFiles.add(s); 
-//					System.out.println(s);
-				} 
+			if (is != null && isr.ready()) {
+				BufferedReader br = new BufferedReader(isr);
+				String s;
+				while ((s = br.readLine()) != null) {
+					ignoreFiles.add(s);
+				}
 				isr.close();
-				is.close(); 
-			}
-			else
+				is.close();
+			} else {
 				System.out.println("ignore.txt not found");
-				
+			}
+
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return ignoreFiles;
 	}
 
+	private String getProtoType(Class<?> pClass) {
+		return getProtoType(pClass.getSimpleName());
+	}
+
 	private String getProtoType(String type) {
-		
-		return map .get(type);
+		return map.get(type.toLowerCase());
 	}
 
 	private String dirToPack(String dir) {
 		return dir.replace("/", ".");
 	}
-	
+
 	private String packToDir(String packageName) {
 		return packageName.replace(".", "/");
 	}
-	
 
 }
